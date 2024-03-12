0d18513d8e2c791da6b27d5948dbc497a1db8844
ErenTerakye <erenterakye@gmail.com>
2024-03-12 21:02:57 +0300

    Create event listener for making the calculations

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let firstNum;
 let operator;
 let secondNum;
+let currentNum = "";
 
 function add(firstNum, secondNum) {
   return firstNum + secondNum;
@@ -19,18 +20,44 @@ function divide(firstNum, secondNum) {
 }
 
 function operate(operator, firstNum, secondNum) {
-  switch(operator) {
-    case "+":
-      add(firstNum, secondNum);
-      break;
-    case "-":
-      subtract(firstNum, secondNum);
-      break;
-    case "*":
-      multiply(firstNum, secondNum);
-      break;
-    case "/":
-      multiply(firstNum, secondNum);
-      break;
+  if (operator === "+") {
+    return firstNum + secondNum;
+  } else if (operator === "-") {
+    return firstNum - secondNum;
+  } else if (operator === "*") {
+    return firstNum * secondNum;
+  } else if (operator === "/") {
+    return firstNum / secondNum;
+  } else {
+    return "ERROR";
   }
 }
+
+const container = document.querySelector("#container");
+const display = document.querySelector("#display");
+container.addEventListener("click", (event) => {
+  if (event.target.className === "number") {
+    currentNum += event.target.textContent;
+    display.textContent = currentNum;
+  } else if (event.target.className === "operator") {
+    if (event.target.textContent !== "=") {
+      operator = event.target.textContent;
+    }
+    console.log(typeof(operator))
+    if (firstNum === undefined) {
+      firstNum = Number(currentNum);
+      currentNum = "";
+    } else if (secondNum === undefined) {
+      secondNum = Number(currentNum);
+      currentNum = "";
+      if (event.target.textContent = "=") {
+        display.textContent = operate(operator, firstNum, secondNum);
+      }
+    }
+  } else if (event.target.className === "clear") {
+    display.textContent = 0;
+    firstNum = undefined;
+    secondNum = undefined;
+    operator = undefined;
+  }
+});
